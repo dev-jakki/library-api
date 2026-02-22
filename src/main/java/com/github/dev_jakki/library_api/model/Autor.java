0d3956f6ac640281b5f9f27c7a7d809a72dbb1a6
dev-jakki <jackcielfelix.dev@gmail.com>
2026-02-22 17:41:40 -0300

    feat: subindo repo

---
+++ src/main/java/com/github/dev_jakki/library_api/model/Autor.java
@@ -0,0 +1,40 @@
+package com.github.dev_jakki.library_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Data
+@ToString(exclude = "livros")
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy =  GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+     @OneToMany( // o fetch padrão aqui é Lazy, pois é OneToMany
+             mappedBy = "autor",
+             cascade = CascadeType.ALL,
+             fetch = FetchType.LAZY
+     )
+//    @Transient
+    private List<Livro> livros;
+}
---
+++ src/main/java/com/github/dev_jakki/library_api/model/GeneroLivro.java
@@ -0,0 +1,10 @@
+package com.github.dev_jakki.library_api.model;
+
+public enum GeneroLivro {
+    FICCAO,
+    FANTASIA,
+    MISTERIO,
+    ROMANCE,
+    BIOGRAFIA,
+    CIENCIA
+}
